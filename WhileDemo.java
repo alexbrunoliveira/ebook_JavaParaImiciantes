b2f21d36b5f9ce8b82b1307a1d078b975a2b82a3
Alex Bruno de Oliveira <alexbrunoliveira@gmail.com>
2022-08-16 13:32:29 -0300

    Instruções de controle

---
+++ WhileDemo.java
@@ -0,0 +1,20 @@
+package com.livro;
+
+public class WhileDemo {
+
+  public static void main(String[] args) {
+  
+  char ch;
+  
+  // exibe o alfabeto
+  ch = 'a';
+  
+  while (ch <='z'){
+    System.out.print(ch);
+    
+    ch++;
+  }
+
+  }
+
+}
